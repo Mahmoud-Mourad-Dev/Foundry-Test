e1ae048b414f3dc41f4a563842ae252ce0be1259
Mahmoud Mourad <mahmoud.mourad.dev@gmail.com>
2024-02-13 20:59:31 +0200

    Add files via upload

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Counter  counter;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function test_Increment() public {
+    function testIncrement() public {
         counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        assertEq(counter.count(), 1);
     }
 }
